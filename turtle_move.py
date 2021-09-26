446716556ec29abe7d4c7acbcf7af357e7473102
Mohamed Emad <mea8538@g.rit.edu>
2021-09-26 14:55:29 +0400

    Initial commit

---
+++ turtle_move.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+import turtle
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def turtle_move(x, y):
+    "this function moved the turtle to a location provided by the caller"
+    turtle.goto(x, y)
+
+
+def main():
+    turtle_move(50, 60)
+
+
+
+main()
